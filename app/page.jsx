8a993768a34b9dfc74f4a1e243eef1d90044d9cf
andrew <booleanorama@gmail.com>
2024-10-12 11:32:09 -0400

    add appwrite config

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,16 @@
+import rooms from "@/data/rooms.json";
+import RoomCard from "@/components/RoomCard";
+import Heading from "@/components/Heading";
+
 export default function Home() {
-  return <div>HOME PAGE</div>;
+  return (
+    <>
+      <Heading title="Available Rooms" />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (
+        <p>No rooms available at the moment </p>
+      )}
+    </>
+  );
 }
