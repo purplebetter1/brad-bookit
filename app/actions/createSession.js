ecbc18b23cee56898063f2ca073e45809506a9bd
andrew <booleanorama@gmail.com>
2024-10-13 13:04:52 -0400

    add toastify

--- app/actions/createSession.js
+++ app/actions/createSession.js
@@ -1,5 +1,18 @@
 "use server";
 
-async function createSession(formData) {}
+async function createSession(previousState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  if (!email || !password) {
+    return {
+      error: "Please fill in all info",
+    };
+  }
+
+  return {
+    success: true,
+  };
+}
 
 export default createSession;
