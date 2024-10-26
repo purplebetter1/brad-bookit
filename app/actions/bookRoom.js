21a607ac55be9d65a7e08b9d426af05539730836
andrew <booleanorama@gmail.com>
2024-10-25 21:29:24 -0400

    check or booking overlap

--- app/actions/bookRoom.js
+++ app/actions/bookRoom.js
@@ -3,14 +3,15 @@ import { createSessionClient } from "@/config/appwrite";
 import { cookies } from "next/headers";
 import { ID } from "node-appwrite";
 import { redirect } from "next/navigation";
-import { revalidatePath } from 'next/cache'
+import { revalidatePath } from "next/cache";
 import checkAuth from "./checkAuth";
+import checkRoomAvailability from "./checkRoomAvailability";
 
 async function bookRoom(previousState, formData) {
   const sessionCookie = cookies().get("appwrite-session");
 
   if (!sessionCookie) {
-    redirect("/");
+    redirect("/login");
   }
 
   try {
@@ -29,16 +30,30 @@ async function bookRoom(previousState, formData) {
     const checkInTime = formData.get("check_in_time");
     const checkOutDate = formData.get("check_out_date");
     const checkOutTime = formData.get("check_out_time");
+    const roomId = formData.get("room_id");
 
     // Combine data and time to ISO 8601
     const checkInDateTime = `${checkInDate}T${checkInTime}`;
     const checkOutDateTime = `${checkOutDate}T${checkOutTime}`;
 
+    // Check if room is available
+    const isAvailable = await checkRoomAvailability(
+      roomId,
+      checkInDateTime,
+      checkOutDateTime
+    );
+
+    if (!isAvailable) {
+      return {
+        error: "Room is already booked for this time",
+      };
+    }
+
     const bookingData = {
       check_in: checkInDateTime,
       check_out: checkOutDateTime,
       user_id: user.id,
-      room_id: formData.get("room_id"),
+      room_id: roomId,
     };
 
     const newBooking = await databases.createDocument(
--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -4,11 +4,12 @@ import getMyBookings from "@/app/actions/getMyBookings";
 
 const BookingsPage = async () => {
   const bookings = await getMyBookings();
+
   return (
     <>
       <Heading title="My Bookings" />
       {bookings.length === 0 ? (
-        <p className="text-gray-600 mt-4">You do not have any bookings yet</p>
+        <p className="text-gray-600 mt-4">You have no bookings</p>
       ) : (
         bookings.map((booking) => (
           <BookedRoomCard key={booking.$id} booking={booking} />
